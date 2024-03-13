a94103ff2b461b09530d7fa0465124de08336050
fahim3221 <fahim the brand>
2024-03-13 11:45:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+
+import { useState } from 'react'
+import './App.css'
+import Blogs from './componends/Blogs/Blogs'
+import Bookmarks from './componends/Bookmarks/Bookmarks'
+import Header from './componends/Header/Header'
+
+function App() {
+
+  const [bookmarks, setBookmarks] = useState([]);
+  const [readingTime, setReadingTime] = useState(0)
+
+  const handleAddToBookmark = blog => {
+    const newBookmarks = [...bookmarks, blog];
+    setBookmarks(newBookmarks);
+  }
+
+  const handleMarkAsRead = (id, time) => {
+    const newReadingTime = readingTime + time;
+    setReadingTime(newReadingTime);
+    // remove the read blog from bookmark
+    const remainingBookmarks = bookmarks.filter(bookmark => bookmark.id !== id);
+    setBookmarks(remainingBookmarks);
+  }
+
+  return (
+    <>
+      <Header></Header>
+      <div className='md:flex max-w-7xl mx-auto'>
+        <Blogs handleAddToBookmark={handleAddToBookmark}
+          handleMarkAsRead={handleMarkAsRead}></Blogs>
+        <Bookmarks bookmarks={bookmarks}
+          readingTime={readingTime}></Bookmarks>
+      </div>
+    </>
+  )
+}
+
+export default App
